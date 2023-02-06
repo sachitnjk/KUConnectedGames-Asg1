b373392bab38a9d805b3b1147b0546a25fb1fb1b
Sachit Nanajkar <sachitnjk@gmail.com>
2023-02-07 00:22:41 +0530

    Added Gradient to health bar

--- KUConnectedGames-Asg1/Assets/Scripts/HealthBarScript.cs
+++ KUConnectedGames-Asg1/Assets/Scripts/HealthBarScript.cs
@@ -7,6 +7,8 @@ public class HealthBarScript : MonoBehaviour
 {
 
 	public Slider healthSlider;
+	public Gradient gradient;
+	public Image fill;
 
 	public void SetMaxHealth(int health)
 	{
@@ -14,11 +16,15 @@ public class HealthBarScript : MonoBehaviour
 		healthSlider.maxValue = health;
 		healthSlider.value = health;
 
+		fill.color = gradient.Evaluate(1f);
+
 	}
 
 	public void SetHealth(int health)
 	{
 		healthSlider.value = health;
+
+		fill.color = gradient.Evaluate(healthSlider.normalizedValue);
 	}
 
 }
