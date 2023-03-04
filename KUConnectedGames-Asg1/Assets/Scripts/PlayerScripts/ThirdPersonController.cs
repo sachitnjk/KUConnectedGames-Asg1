d1d40eac45e0a31f9a9bb508b99f3f8084ed2c2d
Sachit Nanajkar <sachitnjk@gmail.com>
2023-03-05 01:03:22 +0530

    Revert "fixed player movement to be only in camDirection"
    This reverts commit 1bbae8cd29cf76f2743cdf71d57d5696f37777a8.

--- KUConnectedGames-Asg1/Assets/Scripts/PlayerScripts/ThirdPersonController.cs
+++ KUConnectedGames-Asg1/Assets/Scripts/PlayerScripts/ThirdPersonController.cs
@@ -263,17 +263,20 @@ namespace StarterAssets
             // normalise input direction
             Vector3 inputDirection = new Vector3(_input.move.x, 0.0f, _input.move.y).normalized;
 
-			Vector3 lookDirection = _mainCamera.transform.forward;
-			lookDirection.y = 0;
-			Quaternion _targetRotation = Quaternion.LookRotation(lookDirection);
-			float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation.eulerAngles.y, ref _rotationVelocity, RotationSmoothTime);
+            // note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
+            // if there is a move input rotate player when the player is moving
+            if (_input.move != Vector2.zero || _input.isAiming)
+            {
+                _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg +
+                                  _mainCamera.transform.eulerAngles.y;
+                float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity,
+                    RotationSmoothTime);
 
-			// rotate to face camera direction
-			transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+                // rotate to face input direction relative to camera position
+                transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+            }
 
-            //replaced code ^^^
-
-			Vector3 targetDirection = Quaternion.Euler(0.0f,_mainCamera.transform.eulerAngles.y, 0.0f) * inputDirection;
+            Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
 
             // move the player
             _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) +
