681ce9dbfd0412bcfd2bb164bb978d4fc9b3bb8a
Sachit Nanajkar <sachitnjk@gmail.com>
2023-04-03 04:43:26 +0530

    Fixed build errors, build working, ignore warnings

--- KUConnectedGames-Asg1/Assets/Scripts/SoundScripts/BGMHandler.cs
+++ KUConnectedGames-Asg1/Assets/Scripts/SoundScripts/BGMHandler.cs
@@ -1,14 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
+
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class BGMHandler : MonoBehaviour
 {
 	public static BGMHandler instance;
 
-	//int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 	[SerializeField] public AudioSource bgMenusLobby; 
 	private void Awake()
 	{
