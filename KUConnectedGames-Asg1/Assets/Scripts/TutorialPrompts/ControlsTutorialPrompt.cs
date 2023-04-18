96cd3e2eea08dd083ed96d6e757363e8ec185d0a
Sachit Nanajkar <sachitnjk@gmail.com>
2023-04-18 21:28:54 +0530

    CleanUp_added new folders for imported UIElements, ControlsTutorialPrompt_renamed

--- KUConnectedGames-Asg1/Assets/Scripts/TutorialPrompts/ControlsTutorialPrompt.cs
+++ KUConnectedGames-Asg1/Assets/Scripts/TutorialPrompts/ControlsTutorialPrompt.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class ContrlsTurotialPromt : MonoBehaviour
+public class ControlsTutorialPrompt : MonoBehaviour
 {
 	public GameObject tutorialPanel;
 	public float tutorialDisplayTime;
