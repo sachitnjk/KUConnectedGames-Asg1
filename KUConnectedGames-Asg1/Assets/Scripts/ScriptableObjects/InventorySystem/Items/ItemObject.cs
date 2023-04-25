2aa1176937442b3911bacc822523f2dd5f933da2
Sachit Nanajkar <sachitnjk@gmail.com>
2023-04-25 22:44:58 +0530

    InventorySystem_Added InventoryObject SO, ItemObject SO; added DogtagsItem, NotesItem, SamplesItem, Tapesitem SO's from ItemObject SO

---
+++ KUConnectedGames-Asg1/Assets/Scripts/ScriptableObjects/InventorySystem/Items/ItemObject.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+	Samples,
+	Tapes,
+	Notes,
+	Dogtags
+}
+
+[CreateAssetMenu(fileName = "New Item Object", menuName = "Inventory System/Items/ItemObject")]
+public class ItemObject : ScriptableObject
+{
+
+	public GameObject prefab;
+	public ItemType type;
+
+	[TextArea(15, 20)]
+	public string description;
+}
